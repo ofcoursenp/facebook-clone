67cee599f127a454fd99a386d192e6307d7f18eb
ofcoursenp <nexusalt33@gmail.com>
2023-03-04 10:06:48 +0545

    that was close

--- facebook_clone/settings.py
+++ facebook_clone/settings.py
@@ -149,6 +149,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'tgamingand3@gmail.com'
-EMAIL_HOST_PASSWORD = 'nodvynvgfliwqhrg'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 
